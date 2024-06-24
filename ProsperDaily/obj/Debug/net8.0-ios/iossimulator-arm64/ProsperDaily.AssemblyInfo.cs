3506b3b68b7f626f2910d6baaaf107656e197bbe
José Quiñónez <joserobertoq@outlook.com>
2024-06-24 04:29:43 -0600

    Add Entry

--- ProsperDaily/obj/Debug/net8.0-ios/iossimulator-arm64/ProsperDaily.AssemblyInfo.cs
+++ ProsperDaily/obj/Debug/net8.0-ios/iossimulator-arm64/ProsperDaily.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ProsperDaily")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0+08fd2bf9d1fc8e6b774d0affea00f5a7621c3ef5")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0+916a19146177f1aa9f2b6651e111d14514a5211b")]
 [assembly: System.Reflection.AssemblyProductAttribute("ProsperDaily")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ProsperDaily")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
