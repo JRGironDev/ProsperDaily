2868df5b2dfce4d7dc588689229b33df22fb3ddd
José Quiñónez <joserobertoq@outlook.com>
2024-07-01 05:59:18 -0600

    Some fixes

--- ProsperDaily/obj/Debug/net8.0-android/ProsperDaily.AssemblyInfo.cs
+++ ProsperDaily/obj/Debug/net8.0-android/ProsperDaily.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ProsperDaily")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0+f33aa055443a40407d878877b076a1a0b304dc13")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0+05206a9c09d9bd46a895d5bfd249006916e24cf1")]
 [assembly: System.Reflection.AssemblyProductAttribute("ProsperDaily")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ProsperDaily")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
